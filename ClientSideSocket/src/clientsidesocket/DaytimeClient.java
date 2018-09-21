0bf05a1e2a537ef5f6380cec4fb92e9a2b351c25
Travis Peterson <travis@tardisblue.org>
2018-09-20 22:36:45 -0500

    upload

---
+++ ClientSideSocket/src/clientsidesocket/DaytimeClient.java
@@ -0,0 +1,27 @@
+package clientsidesocket;
+
+import java.net.Socket;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class DaytimeClient {
+	public static void main(String[] args) {
+		String hostname = args.length > 0 ? args[0] : "campbell";
+		Socket socket=null;
+		try {
+			socket = new Socket(hostname, 13);
+			socket.setSoTimeout(15000);
+			InputStream in = socket.getInputStream();
+			StringBuilder time = new StringBuilder();
+			InputStreamReader reader = new InputStreamReader(in, "ASCII");
+			for (int c = reader.read(); c != -1; c = reader.read()) time.append((char) c);
+			System.out.println(time);
+		} catch (IOException ex) { System.err.println(ex);
+	} finally {
+		if (socket != null) {
+			try { socket.close(); } catch (IOException ex) {  }
+			}
+		}
+		}
+	}
